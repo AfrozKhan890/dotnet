ef1f4dfbef712ef9225610fe74aa48b9a854da1b
Afroz Khan <Afrozaftab11@gmail.com>
2025-08-26 11:12:34 +0500

    --

--- SymphonyLimited/ViewModels/AdminLoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SymphonyLimited.ViewModels
-{
-    public class AdminLoginViewModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- SymphonyLimited/ViewModels/CourseViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SymphonyLimited.ViewModels
-{
-    public class CourseViewModel
-    {
-        public string Name { get; set; } = string.Empty;
-        public int TopicsCount { get; set; }
-        public DateTime CreatedDate { get; set; }
-    }
-}
